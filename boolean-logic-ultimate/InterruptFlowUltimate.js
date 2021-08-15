5a3298b8e019fe37cff5a5a03840bd95f8afe6a1
Massimo <37666636+Supergiovane@users.noreply.github.com>
2021-08-15 09:49:17 +0200

    .

--- boolean-logic-ultimate/InterruptFlowUltimate.js
+++ boolean-logic-ultimate/InterruptFlowUltimate.js
@@ -6,13 +6,13 @@ module.exports = function (RED) {
 		node.currentMsg = {}; // Stores current payload
 		node.sTriggerTopic = node.config.triggertopic.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '') || "trigger"; // Topic controlling the bInviaMessaggio
 		node.bInviaMessaggio = (node.config.initializewith === undefined || node.config.initializewith === "1") ? true : false; // Send the message or not
-		
+
 		function setNodeStatus({ fill, shape, text }) {
 			var dDate = new Date();
 			node.status({ fill: fill, shape: shape, text: text + " (" + dDate.getDate() + ", " + dDate.toLocaleTimeString() + ")" })
 		}
 		setNodeStatus({ fill: "green", shape: "ring", text: "-> pass" });
-		
+
 
 		if (node.bInviaMessaggio) {
 			setNodeStatus({ fill: "green", shape: "dot", text: "-> pass" });
@@ -24,6 +24,7 @@ module.exports = function (RED) {
 			var sIncomingTopic = "";
 			if (msg.hasOwnProperty("topic")) {
 				// 06/11/2019 
+				if (!msg.hasOwnProperty("topic") || msg.topic === undefined) sIncomingTopic = "NoTopicReceived";
 				sIncomingTopic = msg.topic.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ''); // Cut unwanted Characters
 				if (sIncomingTopic == node.sTriggerTopic) {
 					if (msg.payload !== true && msg.payload !== false) {
@@ -60,19 +61,19 @@ module.exports = function (RED) {
 			}
 		});
 
-		
+
 
 
 		function ToBoolean(value) {
 			var res = false;
 			var decimal = /^\s*[+-]{0,1}\s*([\d]+(\.[\d]*)*)\s*$/
-			
+
 			if (typeof value === 'boolean') {
 				res = value;
 			}
 			else if (typeof value === 'number' || typeof value === 'string') {
 				// Is it formated as a decimal number?
-				
+
 				if (decimal.test(value)) {
 					var v = parseFloat(value);
 					res = v != 0;
